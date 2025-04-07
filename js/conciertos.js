efc646bf7986a305ad9d38a32e9af8f37925bf93
Diego Andres Olivera Abarca <2024_diego.olivera@iticbcn.cat>
2025-04-07 15:48:37 +0200

    dinamically added genres and carrousels

--- js/conciertos.js
+++ js/conciertos.js
@@ -1,22 +1,89 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const tracks = document.querySelectorAll(".concerts-display");
-    const imgWidth = 110;
-    const imgVisible = 5;
-    const imgTotal = 10;
-    
-    tracks.forEach(track => {
-        let index = 0;
-        function moveCarrusel(){
-            index++;
-            if(index > imgTotal - imgVisible){
-                index = 0;
-            }
-            track.style.transform = `translateX(-${index * imgWidth}px)`;
+    fetch("http://127.0.0.1:8000/")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Error with server connection");
         }
-        
-        (function startInterval(){
-            setInterval(moveCarrusel, 2000);
-        })();
+        return response.json();
+    })
+    .then(data => {
+        console.log("Received data from server:", data);
+    })
+    .catch(error => {
+        console.error("Error capturat:", error);
     });
 
+    const mainBodyDiv = document.querySelector(".main-body");
+
+    if(mainBodyDiv){
+        fetch("http://127.0.0.1:8000/generos")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status}}`);
+            }
+            return response.json();
+        })
+        .then(generos => {
+            console.log("Generos recibidos:", generos);
+
+            generos.forEach(genero => {
+                console.log(`Añadiendo genero: ${genero.nombre}`);
+
+                const genContainer = document.createElement("div");
+                genContainer.className = "major-container";
+
+                const genH2 = document.createElement("h2");
+                genH2.textContent = genero.nombre;    
+
+                const genDiv = document.createElement("div");
+                genDiv.className = "concerts-container";
+
+                const displayDiv = document.createElement("div");
+                displayDiv.className = "concerts-display";
+
+                for(let i = 0; i < 10; i++){
+                    const img = document.createElement("img");
+                    img.src = "../img/logo.png";
+                    img.alt = `imagen${i+1}`;
+                    displayDiv.appendChild(img);
+                }
+
+                genDiv.append(displayDiv);
+                genContainer.appendChild(genH2);
+                genContainer.appendChild(genDiv);
+                mainBodyDiv.appendChild(genContainer);
+                const imgWidth = 176;
+                const imgVisible = 5;
+                const imgTotal = 10;
+                const tracks = document.querySelectorAll(".concerts-display");
+            
+                tracks.forEach(track => {
+                    let index = 0;
+                    function moveCarrusel(){
+                        index++;
+                        if(index > imgTotal - imgVisible){
+                            index = 0;
+                        }
+                        track.style.transform = `translateX(-${index * imgWidth}px)`;
+                    }
+                    
+                    (function startInterval(){
+                        setInterval(moveCarrusel, 5000);
+                    })();
+                });
+            });
+        })
+        .catch(error => {
+            console.error("Error al leer generos:", error);
+            try {
+                const errorData = JSON.parse(error.message);
+                if(errorData?.detail){
+                    alert(errorData.detail);
+                    return;
+                }
+            } catch (e) {
+                console.error("Error parsing response:", e);
+            }
+        });
+    }
 });
